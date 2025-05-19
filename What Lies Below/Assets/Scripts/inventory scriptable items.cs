91c58ed4904a2c213ac2a04bdad061c1e269b1cc
boobles-iguess <dbee3183@gmail.com>
2025-05-19 14:12:42 +0200

    Update inventory scriptable items.cs
    for tjhe actual items that are being stored in the input manager, attach these scripts to those objects

--- What Lies Below/Assets/Scripts/inventory scriptable items.cs
+++ What Lies Below/Assets/Scripts/inventory scriptable items.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
-
-public class NewEmptyCSharpScript
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
 {
-    
+    public string itemName;
+    public Sprite icon;
+    public bool isStackable = true;
+}
+[System.Serializable]
+public class InventorySlot
+{
+    public Item item;
+    public int quantity;
+
+    public void ClearSlot()
+    {
+        item = null;
+        quantity = 0;
+    }
 }
