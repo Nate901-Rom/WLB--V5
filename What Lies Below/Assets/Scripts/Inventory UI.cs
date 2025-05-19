c4d7a9f351daae04afd87e41233c284c4b825990
boobles-iguess <dbee3183@gmail.com>
2025-05-19 14:50:39 +0200

    Update Inventory UI.cs

--- What Lies Below/Assets/Scripts/Inventory UI.cs
+++ What Lies Below/Assets/Scripts/Inventory UI.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class InventoryUI
+public class InventorySlotUI : MonoBehaviour
 {
-    
+    public Image icon;
+    public Text quantityText;
+    private int index;
+
+    public void SetupSlot(int i)
+    {
+        index = i;
+    }
+
+    void Update()
+    {
+        var slot = Inventory.Instance.slots[index];
+        if (slot.item != null)
+        {
+            icon.sprite = slot.item.icon;
+            icon.enabled = true;
+            quantityText.text = slot.item.isStackable ? slot.quantity.ToString() : "";
+        }
+        else
+        {
+            icon.enabled = false;
+            quantityText.text = "";
+        }
+    }
 }
